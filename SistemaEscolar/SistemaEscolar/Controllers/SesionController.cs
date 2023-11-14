13c5cdacbaa352dbec48fe119c302eecd2447a90
Carlos Portillo <carloskportillo@gmail.com>
2023-11-13 22:07:13 -0600

    Publicando rama personal

---
+++ SistemaEscolar/SistemaEscolar/Controllers/SesionController.cs
@@ -0,0 +1,90 @@
+﻿using SistemaEscolar.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaEscolar.Controllers
+{
+    public class SesionController
+    {
+        private SesionModel sesionModel;
+        private FrmLogin frmLogin;
+
+        public SesionController(FrmLogin frmLogin)
+        {
+            this.sesionModel = new SesionModel();
+            this.frmLogin = frmLogin;
+
+            frmLogin.btnIngresarAction += OnIniciarSesionButtonClick;
+        }
+
+        public void OnIniciarSesionButtonClick(string usuario, string contrasena)
+        {
+            if (string.IsNullOrEmpty(usuario) && string.IsNullOrEmpty(contrasena))
+            {
+                MessageBox.Show("Debe ingresar su usuario y contraseña", "¡Cuidado!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(contrasena))
+                {
+                    MessageBox.Show("Debe ingresar su contraseña", "¡Cuidado!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(usuario))
+                    {
+                        MessageBox.Show("Debe ingresar su usuario", "¡Cuidado!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        try
+                        {
+                            sesionModel.Nivel = sesionModel.IniciarSesion(usuario, contrasena);
+
+                            if (sesionModel.Nivel == 1)
+                            {
+                                frmLogin.idProfesor = sesionModel.ExtraerID(usuario, contrasena);
+                                frmLogin.nombreProfesor = sesionModel.ExtraerNombre(usuario, contrasena);
+                                frmLogin.fotoPerfilProfesor = sesionModel.ExtraerFoto(usuario, contrasena);
+
+                                MessageBox.Show("Var 1: " + frmLogin.idProfesor.ToString());
+                                MessageBox.Show("Var 2: " + frmLogin.nombreProfesor);
+                                
+                                /*
+                                FrmMenuAdmin formMenuPrueba = new FrmMenuAdmin();
+                                formMenuPrueba.Show();
+                                frmLogin.Hide();
+                                */
+                            }
+                            else if (sesionModel.Nivel == 2)
+                            {
+                                frmLogin.idProfesor = sesionModel.ExtraerID(usuario, contrasena);
+                                frmLogin.nombreProfesor = sesionModel.ExtraerNombre(usuario, contrasena);
+                                frmLogin.fotoPerfilProfesor = sesionModel.ExtraerFoto(usuario, contrasena);
+
+                                /*
+                                MenuProfesor menuProfesor = new MenuProfesor();
+                                menuProfesor.Show();
+                                frmLogin.Hide();
+                                */
+                            }
+                            else
+                            {
+                                MessageBox.Show("Usuario o contraseña incorrecta", "¡Cuidado!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error: " + ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ SistemaEscolar/SistemaEscolar/Models/SesionModel.cs
@@ -0,0 +1,184 @@
+﻿// SesionModel.cs
+using SistemaEscolar.Model;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SistemaEscolar.Model
+{
+    public class SesionModel : Conexion
+    {
+        private int nivel;
+        private String nombre;
+
+        public int Nivel { get => nivel; set => nivel = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+
+        public SesionModel()
+        {
+
+        }
+
+        public SesionModel(int nivel, String nombre)
+        {
+            this.nivel = nivel;
+            this.nombre = nombre;
+        }
+
+        ~SesionModel()
+        {
+            Console.WriteLine("Out..");
+        }
+
+        public class ModelPrueba
+        {
+            public int idProfesor { get; set; }
+            public string nombreProfesor { get; set; }    
+            public Byte[] fotoPerfilProfesor { get; set;}
+
+            public int sesionNivel {  get; set; }
+        }
+
+        public int IniciarSesion(String user, String password)
+        {
+            int idProfesor = 0;
+
+            try
+            {
+                SqlDataReader dr1 = null;
+                SqlCommand comando = new SqlCommand();
+                comando.Connection = this.Conectar();
+                comando.CommandText = "ps_mostrar_nivel_usuario";
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@usuario", user);
+                comando.Parameters.AddWithValue("@contra", password);
+                dr1 = comando.ExecuteReader();
+
+                while (dr1.Read())
+                {
+                    idProfesor = int.Parse(dr1["id_Nivel"].ToString());
+                }
+
+                if (dr1 != null)
+                {
+                    Console.WriteLine("Usuario encontrado");
+                }
+
+                this.Desconectar();
+                dr1.Close();
+                return idProfesor;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error al iniciar sesión " + e.Message);
+            }
+
+            return idProfesor;
+        }
+
+        public String ExtraerNombre(String user, String password)
+        {
+            String nombreProfesor = String.Empty;
+            try
+            {
+
+                SqlDataReader dr1 = null;
+                SqlCommand comando = new SqlCommand();
+                comando.Connection = this.Conectar();
+                comando.CommandText = "ps_mostrar_nombre_profesor";
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@usuario", user);
+                comando.Parameters.AddWithValue("@contra", password);
+                dr1 = comando.ExecuteReader();
+
+                while (dr1.Read())
+                {
+                    nombreProfesor = dr1["nombreProfesor"].ToString().Trim() + " " + dr1["apellidoProfesor"].ToString().Trim();
+                }
+                if (dr1 != null)
+                {
+                    Console.WriteLine("Datos encontrados");
+                }
+                this.Desconectar();
+                dr1.Close();
+                return nombreProfesor;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error al inicar sesión " + e.Message);
+            }
+            return nombreProfesor;
+        }
+
+        public int ExtraerID(String user, String password)
+        {
+            int id_Profesor = 0;
+
+            try
+            {
+                SqlDataReader dr1 = null;
+                SqlCommand comando = new SqlCommand();
+                comando.Connection = this.Conectar();
+                comando.CommandText = "ps_mostrar_id_profesor";
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@usuario", user);
+                comando.Parameters.AddWithValue("@contra", password);
+                dr1 = comando.ExecuteReader();
+
+                while (dr1.Read())
+                {
+                    id_Profesor = int.Parse(dr1["id_Profesor"].ToString());
+                }
+                if (dr1 != null)
+                {
+                    Console.WriteLine("Datos encontrados");
+                }
+                this.Desconectar();
+                dr1.Close();
+                return id_Profesor;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error al inicar sesión " + e.Message);
+            }
+            return id_Profesor;
+        }
+
+        public byte[] ExtraerFoto(String user, String password)
+        {
+            Byte[] data = new Byte[0];
+            try
+            {
+                SqlDataReader dr1 = null;
+                SqlCommand comando = new SqlCommand();
+                comando.Connection = this.Conectar();
+                comando.CommandText = "ps_mostrar_foto_profesor";
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@usuario", user);
+                comando.Parameters.AddWithValue("@contra", password);
+                dr1 = comando.ExecuteReader();
+
+                if (dr1 != null)
+                {
+                    while (dr1.Read())
+                    {
+                        data = (Byte[])(dr1["fotoPerfilProfesor"]);
+                    }
+                    if (dr1 != null)
+                    {
+                        Console.WriteLine("Datos encontrados");
+                    }
+                }
+
+                this.Desconectar();
+                dr1.Close();
+                return data;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error al extraer foto " + e.Message);
+            }
+            return data;
+        }
+    }
+}
